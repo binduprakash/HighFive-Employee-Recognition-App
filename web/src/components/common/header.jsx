448067fe80c59ee00f5d4a0be935e4826082ba42
Madhuri Chalamangalam <madhuri.chalamangalam@gmail.com>
2019-03-31 13:38:57 -0500

    Added Reward Component

--- web/src/components/common/header.jsx
+++ web/src/components/common/header.jsx
@@ -5,14 +5,14 @@ import '../../styles/navbar.css';
 class Overview extends Component {
   render() {
     return (
-      <header class="header">
-        <h1 class="logo">
+      <header className="header">
+        <h1 className="logo">
           {/* eslint-disable react/jsx-no-target-blank */}
           <a href="/">
             <img src="../public/high_five_logo.png" alt="High-Five" />
           </a>
         </h1>
-        <ul class="main-nav">
+        <ul className="main-nav">
           <li>Points Available: 2,000</li>
         </ul>
       </header>
--- web/src/components/overview/recent-rewards.jsx
+++ web/src/components/overview/recent-rewards.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Reward from './reward';
+
 class RecentRewards extends Component {
   render() {
     return (
       <div className="RecentRewards">
-        <h3>RecentRewards Code Goes Here</h3>
+        <Reward giver={'Bindu'} recipient={'Maddie'} points={20} message={'Great job on the High-Five project'} />
+        <Reward giver={'Maddie'} recipient={'Tyler'} points={20} message={'Great job on the High-Five project'} />
+        <Reward giver={'Tyler'} recipient={'Maddie'} points={20} message={'Great job on the High-Five project'} />
       </div>
     );
   }
