2fa2dcf42aacfd2da037c92eda13a5c7b37e97cd
Bindu Prakash <bindu2305@gmail.com>
2019-03-31 22:11:33 +0000

    Added a sample get request for employees

--- web/src/components/recognize.jsx
+++ web/src/components/recognize.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import API from '../api';
+
 class Recognize extends React.Component {
+  state = {
+    employees: []
+  }
+  componentDidMount() {
+    API.get('employees').then(res => {
+        const employees = res.data;
+        this.setState({ employees });
+    })
+  }
   render() {
     return (
       <div className="Recognize">
         <h1>Recognize</h1>
+        <ul>
+          { this.state.employees.map(employee => <li key={employee.id}>{employee.first_name},{employee.last_name} </li>)}
+        </ul>
       </div>
     )
   }
