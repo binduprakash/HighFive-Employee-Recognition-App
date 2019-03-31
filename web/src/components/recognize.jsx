4aa74ead3d45abe0ff6e178d5d357b6bef190db6
Tyler <tyler.tomczyk@gmail.com>
2019-03-31 03:46:03 +0000

    initial recognize employee form set up

--- web/src/components/recognize.jsx
+++ web/src/components/recognize.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import NavBar from "./navbar.jsx"
+import Form from './recognizeform.jsx';
 
 require('../styles/navbar.css')
+require('../styles/form.css')
 
 class Recognize extends React.Component {
   render() {
     return (
       <div>
         < NavBar />
-        <h1>Recognize</h1>
+        <h1>Recognize a Co-worker!</h1>
+        < Form />
       </div>
     )
   }
---
+++ web/src/components/recognizeform.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+
+// Create component for button
+class Button extends React.Component {
+    render() {
+      return (
+        <fieldset>
+          <button
+            type={this.props.type || 'button'}
+            value={this.props.value || null}
+          >
+            {this.props.text}
+          </button>
+        </fieldset>
+      );
+    }
+  };
+  
+  // Create component for label
+  class Label extends React.Component {
+    render() {
+      if (this.props.hasLabel === 'true') {
+        return <label htmlFor={this.props.htmlFor}>{this.props.label}</label>
+      }
+    }
+  }
+  
+  // Create component for select input
+  class Select extends React.Component {
+    render() {
+      // Get all options from option prop
+      const selectOptions = this.props.options.split(', ');
+  
+      // Generate list of options
+      const selectOptionsList = selectOptions.map((selectOption, index) => {
+        return <option key={index} value={index}>{selectOption}</option>
+      });
+  
+      return (
+        <fieldset>
+          <Label
+            hasLabel={this.props.hasLabel}
+            htmlFor={this.props.htmlFor}
+            label={this.props.label}
+          />
+          
+          <select
+            defaultValue=''
+            id={this.props.htmlFor}
+            name={this.props.name || null}
+            required={this.props.required || null}
+          >
+            <option value='' disabled>Select one option</option>
+  
+            {selectOptionsList}
+          </select>
+        </fieldset>
+      );
+    }
+  };
+  
+  // Create component for textarea
+  class Textarea extends React.Component {
+    render() {
+      return (
+        <fieldset>
+          <Label
+            hasLabel={this.props.hasLabel}
+            htmlFor={this.props.htmlFor}
+            label={this.props.label}
+          />
+  
+          <textarea
+            cols={this.props.cols || null}
+            id={this.props.htmlFor}
+            name={this.props.name || null}
+            required={this.props.required || null}
+            rows={this.props.rows || null}
+          >
+          </textarea>
+        </fieldset>
+      );
+    }
+  };
+  
+  // Create component for form
+  class Form extends React.Component {
+    render() {
+      return (
+        <form method='' action=''>
+         <Select
+            hasLabel='true'
+            htmlFor='select'
+            label='Please Select Employee to Recognize'
+            options='Tyler, Bindu, Maddie'
+            required='true'
+          />
+          
+          
+          <Select
+            hasLabel='true'
+            htmlFor='select'
+            label='Option 1 - Please Select Points'
+            options='one, two, three, option four, five'
+            required='true'
+          />
+          
+          <Textarea
+            hasLabel='true'
+            htmlFor='textarea'
+            label='Message to Employee'
+            required='true'
+            rows='10'
+          />
+          
+          <Textarea
+            hasLabel='true'
+            htmlFor='textarea'
+            label='Message to Manager'
+            required='true'
+            rows='10'
+          />
+          
+          <Button
+            type='submit'
+            value='submit'
+            text='Send form'
+          />
+        </form>
+      )
+    }
+  }
+export default Form
