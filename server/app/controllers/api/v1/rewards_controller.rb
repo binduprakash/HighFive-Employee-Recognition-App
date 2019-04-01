3d63dae335a0cbe2157a33e1345915172c1a3001
Tyler <tyler.tomczyk@gmail.com>
2019-04-01 04:20:13 +0000

    small edit to save to rewards_controller.rb - uncomment out a line

--- server/app/controllers/api/v1/rewards_controller.rb
+++ server/app/controllers/api/v1/rewards_controller.rb
@@ -35,7 +35,7 @@ class Api::V1::RewardsController < ApplicationController
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_reward
-    #    @reward = Reward.find(params[:id])
+    @reward = Reward.find(params[:id])
     end
 
     # Only allow a trusted parameter “white list” through.
