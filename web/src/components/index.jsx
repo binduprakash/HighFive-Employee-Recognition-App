aa16c8b3d6b0beb652a92c6cd7f1266989b1923f
Bindu Prakash <bindu2305@gmail.com>
2019-04-07 07:43:23 +0000

    Refactored NavBar to sepearte component

--- web/src/components/index.jsx
+++ web/src/components/index.jsx
@@ -5,40 +5,10 @@ import Routes from './Routes'
 import { withCookies, Cookies } from 'react-cookie';
 import { compose } from 'recompose';
 import API from '../api';
+import NavBar from './common/NavBar';
 
-require('../styles/navbar.css')
 require('../styles/login.css')
 
-
-class NavBar extends Component {
-  render() {
-    const {showLogin, handleLogout, currentPage} = this.props;
-    return (
-      <div className="header_2">
-        <ul className="main-nav">
-          <li>
-            <Link className={currentPage === 'overview' ? 'main-nav-active': 'main-nav-inactive'} to="/overview">Overview</Link>
-          </li>
-          <li>
-            <Link className={currentPage === 'recognize' ? 'main-nav-active': 'main-nav-inactive'} to="/recognize">Recognize</Link>
-          </li>
-          <li>
-            <Link className={currentPage === 'redeem' ? 'main-nav-active': 'main-nav-inactive'} to="/redeem">Redeem Rewards</Link>
-          </li>
-          <li>
-            <Link className={currentPage === 'rewards_activities' ? 'main-nav-active': 'main-nav-inactive'} to="/rewards_activities">Rewards History</Link>
-          </li>
-          <li>
-            {showLogin
-              ? <Link className={currentPage === 'login' ? 'main-nav-active': 'main-nav-inactive'} to="/login" onClick={handleLogout}>Logout</Link>
-              : <Link  className={currentPage === 'login' ? 'main-nav-active': 'main-nav-inactive'} to="/login">Login</Link>
-            }
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
 
