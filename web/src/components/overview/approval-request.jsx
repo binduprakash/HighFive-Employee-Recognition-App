f1e1bab50250c91fc21ea7a11aa9740519c8767d
Bindu Prakash <bindu2305@gmail.com>
2019-04-10 05:04:19 +0000

    Improved approval section with react bootstrap buttons and layout

--- web/src/components/overview/approval-request.jsx
+++ web/src/components/overview/approval-request.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
+import {Button} from 'react-bootstrap';
 import '../../styles/approval-request.css'
 
 const approval = ({ id, from, to, level, reward_message, approver_message, on_approve, on_reject }) => (
     <div className="approval">
-        <p>{from.first_name + ' ' + from.last_name} gave {to.first_name + ' ' + to.last_name} {level.points} points </p>
-        <p className="approval-message">{reward_message}</p>
-        <p className="approval-request-message">{approver_message}</p>
+        <p><b>{from.first_name + ' ' + from.last_name}</b> recognized <b>{to.first_name + ' ' + to.last_name}</b> with {level.points} points </p>
+        Message to you:<p className="approval-request-message">{approver_message}</p>
         <div className="actions">
-            <a href="javascript:void(0)" className="primary" onClick={(e) => { on_approve(id)}}>Approve</a>
-            <a href="javascript:void(0)" className="secondary" onClick={(e) => { on_reject(id)}}>Reject</a>
+            <Button variant="success" size='sm' onClick={(e) => { on_approve(id)}}>Approve</Button>
+            <Button variant="danger" size='sm' onClick={(e) => { on_reject(id)}} style={{'margin-left': '10px'}}>Reject</Button>
         </div>
     </div>
 )
